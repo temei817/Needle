3c39b12ceb376b3b7148d8822aaee70834a20b16
“[Teresa <[teresamei935@gmail.com]>
2018-07-02 10:09:32 -0700

    Initial commit

---
+++ android/src/main/java/com/missionbit/game/android/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package com.missionbit.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.missionbit.game.Needle;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        initialize(new Needle(), configuration);
+    }
+}
---
+++ core/src/main/java/com/missionbit/game/Needle.java
@@ -0,0 +1,7 @@
+package com.missionbit.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Needle extends ApplicationAdapter {
+}
