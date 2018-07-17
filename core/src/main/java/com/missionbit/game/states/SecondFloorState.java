0075d22cf9d42a116c0425bd7186de850a907ae9
Michelle <kwong.meshell@gmail.com>
2018-07-17 12:36:48 -0700

    Added walls to secondfloor

--- core/src/main/java/com/missionbit/game/states/SecondFloorState.java
+++ core/src/main/java/com/missionbit/game/states/SecondFloorState.java
@@ -3,14 +3,31 @@ package com.missionbit.game.states;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector3;
 import com.missionbit.game.Needle;
+import com.missionbit.game.PolygonButton;
 import com.missionbit.game.characters.Female;
+
+import java.util.ArrayList;
 
 public class SecondFloorState extends State{
 
     private Texture bkgd;
     private Female female;
+    private float[][] wall = new float[][]{
+            {33.0f, 98.0f, 2.0f, 64.0f, 3.0f, 232.0f, 28.0f, 261.0f, 33.0f, 94.999985f, },
+            {32.0f, 265.0f, 148.0f, 264.0f, 149.0f, 98.0f, 164.0f, 98.0f, 232.0f, 175.0f, 825.0f, 176.0f, 825.0f, 339.0f, 914.0f, 338.0f, 912.0f, 176.0f, 958.0f, 176.0f, 958.0f, 376.0f, 3.0f, 381.0f, 2.0f, 235.0f, 29.0f, 264.0f, },
+    };
+    private ArrayList<PolygonButton> walls;
+    private boolean showDebug = true;
+    private PolygonButton basementDoor;
+    private float[] basementDoorVertices ={33.0f, 260.0f, 34.0f, 96.0f, 148.0f, 97.0f, 148.0f, 264.0f, 33.0f, 263.0f};
+
+
+    private ShapeRenderer debugRenderer = new ShapeRenderer();
+
+
 
 
     public SecondFloorState(GameStateManager gsm) {
@@ -19,6 +36,14 @@ public class SecondFloorState extends State{
         bkgd = new Texture("images/Secondfloor.png");
         female = new Female(50, 50);
 
+        //walls
+        walls = new ArrayList<PolygonButton>();
+        for(int i=0;i<wall.length;i++){
+            walls.add(new PolygonButton(wall[i]));
+        }
+
+        //doors
+        basementDoor = new PolygonButton(basementDoorVertices);
     }
 
     @Override
@@ -27,7 +52,13 @@ public class SecondFloorState extends State{
             Vector3 touchPos = new Vector3();
             touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
             cam.unproject(touchPos);
-            female.setTargetLoc((int) touchPos.x, (int) touchPos.y);
+
+            if(basementDoor.handleClick(touchPos)){
+                gsm.pop();
+            }
+            else{
+                female.setTargetLoc((int) touchPos.x, (int) touchPos.y);
+            }
         }
 
     }
@@ -35,7 +66,7 @@ public class SecondFloorState extends State{
     @Override
     public void update(float dt) {
         handleInput();
-        //female.update(dt);
+        female.update(dt,walls);
 
         //camera bounds
         float minX = cam.viewportWidth / 2, maxX = bkgd.getWidth() - cam.viewportWidth / 2;
@@ -71,6 +102,18 @@ public class SecondFloorState extends State{
         }
 
         sb.end();
+
+        if (showDebug) {
+            debugRenderer.setProjectionMatrix(cam.combined);
+            debugRenderer.begin(ShapeRenderer.ShapeType.Line);
+            debugRenderer.setColor(1, 1, 1, 1);
+            for(PolygonButton wall:walls){
+                wall.drawDebug(debugRenderer);
+            }
+            basementDoor.drawDebug(debugRenderer);
+        }
+        debugRenderer.end();
+
 
     }
 
