d8dabe7ba08bff825958e15cadbe372dbc66e3d5
“[Teresa <[teresamei935@gmail.com]>
2018-07-18 09:16:55 -0700

    animation stuff

--- core/src/main/java/com/missionbit/game/Animations.java
+++ core/src/main/java/com/missionbit/game/Animations.java
@@ -1,5 +1,7 @@
 package com.missionbit.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.Array;
 
@@ -10,7 +12,27 @@ public class Animations{
     float currentFrameTime; //how long the animation has been in the current frame
     int frameCount; //number of frames in our animation
     int frame; //the current frame we're in
+    public Animations(String path, int cols, int rows, int framecount, float cycleTime){
+        frames = new Array<TextureRegion>();
+        Texture Sheet = new Texture(Gdx.files.internal(path));
+        TextureRegion[][] tmp = TextureRegion.split(Sheet,
+                Sheet.getWidth() / cols,
+                Sheet.getHeight() / rows);
 
+        int index = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (index < frame) {
+                    frames.add(tmp[i][j]);
+
+                }
+            }
+
+        }
+        this.frameCount = frameCount;
+        maxFrameTime = cycleTime / frameCount;
+        frame = 0;
+    }
     public Animations(TextureRegion region, int frameCount, float cycleTime){
         frames = new Array<TextureRegion>();
         int frameWidth = region.getRegionWidth() / frameCount;
