c9c9069fab47f5162ccb28bb9aa3733106fd0712
Garrett <choy66woy@gmail.com>
2018-07-24 10:59:35 -0700

    same

--- core/src/main/java/com/missionbit/game/states/GameOverState.java
+++ core/src/main/java/com/missionbit/game/states/GameOverState.java
@@ -24,7 +24,7 @@ public class GameOverState extends State {
         badEnding3 = new Animations("images/Badendng.png",3,7,20,1f,false);
         escape0 = new Animations("images/escape0.png",3,6,17,1f,false);
         escape1 = new Animations("images/escape1.png",3,5,15,0.3f,false);
-        escape2 = new Animations("images/escape2.png",3,6,24,0.6f,false);
+        escape2 = new Animations("images/escape2.png",3,8,24,0.6f,false);
         escape3 = new Animations("images/escape3.png",4,8,30,1f,false);
         goodendmusic = Gdx.audio.newMusic(Gdx.files.internal("Music/Hollow_Knight_OST_White_Palace.mp3"));
         badendmusic = Gdx.audio.newMusic(Gdx.files.internal("Music/Rain_World_Threat_Chimney_Canopy_Soundtrack_OST_.mp3"));
