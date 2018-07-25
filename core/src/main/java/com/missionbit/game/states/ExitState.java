e4575fb4c5219f7c6d76cc8c9d6dfe440e61885f
Michelle <kwong.meshell@gmail.com>
2018-07-25 11:42:00 -0700

    added exit state

--- core/src/main/java/com/missionbit/game/states/ExitState.java
+++ core/src/main/java/com/missionbit/game/states/ExitState.java
@@ -1,9 +1,34 @@
 package com.missionbit.game.states;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class ExitState {
-    private Texture Exit
+public class ExitState extends State{
+    private Texture Exit;
+
+    public ExitState(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    protected void handleInput() {
+        
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
 
 
